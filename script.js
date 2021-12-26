033042801dc5fe047b23a2e1d112ca40d3f94f98
anon123123123 <cditodavis96@gmail.com>
2021-12-25 17:33:14 -0700

    Bug fix: switched form to click to avoid refresh issue

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+async function writePassword(length, lowerCase, upperCase, numbers, specialChars) {
+  var password = await generatePassword(length, lowerCase, upperCase, numbers, specialChars);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -15,34 +15,38 @@ generateBtn.addEventListener("click", displayPrompt);
 
 // Password Generator 
 async function generatePassword(length, lowerCase, upperCase, numbers, specialChars) {
-  // Blank vars to be filled 
-  var chars = "";
-  var password = "";
-  let base_char = "abcdefghijklmnopqrstuvwxyz"
-  // IFs check options and create char set 
-  if (lowerCase === true) {
-    chars += base_char
-  } 
-  if (upperCase === true) {
-    let upCase = base_char.toUpperCase()
-    chars += upCase
-  }
-  if (numbers === true) {
-    chars += '0123456789'
-  }
-  if (specialChars === true) {
-    chars += '!@#$%^&*()'
-  }
-  for (let i = 0; i <= length; i++) {
-    let randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
-   }
-   console.log(password)
+  return new Promise(resolve => {
+    // Blank vars to be filled 
+    var chars = "";
+    var password = "";
+    let base_char = "abcdefghijklmnopqrstuvwxyz"
+    // IFs check options and create char set 
+    if (lowerCase === true) {
+      chars += base_char
+    }
+    if (upperCase === true) {
+      let upCase = base_char.toUpperCase()
+      chars += upCase
+    }
+    if (numbers === true) {
+      chars += '0123456789'
+    }
+    if (specialChars === true) {
+      chars += '!@#$%^&*()'
+    }
+    for (let i = 0; i <= length; i++) {
+      let randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.querySelector('#password-wizard-prompt').classList.toggle('hidden')
+    console.log(password)
+    resolve(password)
+  });
 }
 
 // Display Custom Form
-function displayPrompt(){
-  document.querySelector('#password-wizard-prompt').classList.remove('hidden')
+function displayPrompt() {
+  document.querySelector('#password-wizard-prompt').classList.toggle('hidden')
 }
 
 // Character Length Slider Controller 
@@ -50,17 +54,17 @@ const slider = document.getElementById("pass-length-slider");
 let output = document.getElementById("pass-length");
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
 }
 
 // Password Options   
-var pass_form = document.getElementById('password-wiz-form')
-pass_form.onsubmit = function() {
-  let length = pass_form['pass-length-slider'].value
-  let lowerCase = pass_form['lower-case'].checked
-  let upperCase = pass_form['upper-case'].checked
-  let numbers = pass_form['numbers'].checked
-  let specialChars = pass_form['special-chars'].checked
-  generatePassword(length, lowerCase, upperCase, numbers, specialChars)
+let pass_form = document.getElementById('password-conf')
+pass_form.onclick = function () {
+  let length = document.getElementById('pass-length-slider').value
+  let lowerCase = document.getElementById('lower-case').checked
+  let upperCase = document.getElementById('upper-case').checked
+  let numbers = document.getElementById('numbers').checked
+  let specialChars = document.getElementById('special-chars').checked
+  writePassword(length, lowerCase, upperCase, numbers, specialChars)
 }
